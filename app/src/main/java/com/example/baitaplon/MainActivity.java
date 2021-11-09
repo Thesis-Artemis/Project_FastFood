07b88098315b037d6ddb2403af92e3d912e40b16
duchunghp444 <hunghuynh2k@gmail.com>
2021-11-10 01:43:15 +0700

    "commit man hinh 3"

--- app/src/main/java/com/example/baitaplon/MainActivity.java
+++ app/src/main/java/com/example/baitaplon/MainActivity.java
@@ -1,14 +1,14 @@
 package com.example.baitaplon;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class screen3 extends AppCompatActivity {
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.screen3);
+        setContentView(R.layout.activity_main);
     }
 }
